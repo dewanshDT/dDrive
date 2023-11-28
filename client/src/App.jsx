74db7b41f9f2e101342b552cd5d1cebd02aee0e8
dewanshDT <dewansh.dt@gmail.com>
2023-11-28 12:52:26 +0530

    login ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,63 @@
-import './App.css'
-import FileUploadDownload from './components/FileUploadDownload'
+import { useState } from "react"
+import "./App.css"
+import FileUploadDownload from "./components/FileUploadDownload"
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = () => {
+    if (username && password) {
+      if (username === "dewansh.dt@gmail.com" && password === "root")
+        setLoggedIn(true)
+      else alert("Username or password incorrect")
+
+      setUsername("")
+      setPassword("")
+    } else {
+      alert("Please enter a username and a password.")
+    }
+  }
+
+  const handleLogout = () => {
+    setLoggedIn(false)
+    setUsername("")
+    setPassword("")
+  }
 
   return (
     <>
-      <FileUploadDownload />
+      {isLoggedIn ? (
+        <FileUploadDownload handleLogout={handleLogout} />
+      ) : (
+        <div className="container login">
+          <div className="login-component">
+            <h6>Login</h6>
+            <div>
+              <input
+                type="email"
+                id="username"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <br />
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button onClick={handleLogin}>Login</button>
+          </div>
+          <h5>
+            Remote<strong>NAS</strong>
+          </h5>
+        </div>
+      )}
     </>
   )
 }
--- client/src/components/FileUploadDownload.jsx
+++ client/src/components/FileUploadDownload.jsx
@@ -1,73 +1,82 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { AiOutlineCloudUpload, AiTwotoneFile } from 'react-icons/ai'
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { AiOutlineCloudUpload, AiTwotoneFile } from "react-icons/ai"
 
-const FileUploadDownload = () => {
-  const [file, setFile] = useState(null);
-  const [uploadedFiles, setUploadedFiles] = useState([]);
+// eslint-disable-next-line react/prop-types
+const FileUploadDownload = ({ handleLogout }) => {
+  const [file, setFile] = useState(null)
+  const [uploadedFiles, setUploadedFiles] = useState([])
 
   useEffect(() => {
-    // Fetch the list of uploaded files from the server
-    axios.get('http://localhost:3001/uploads')
+    axios
+      .get("http://localhost:3001/uploads")
       .then((response) => {
-        setUploadedFiles(response.data);
+        setUploadedFiles(response.data)
       })
       .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+        console.error(error)
+      })
+  }, [])
 
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
+    setFile(e.target.files[0])
+  }
 
   const handleUpload = () => {
-    const formData = new FormData();
-    formData.append('file', file);
+    const formData = new FormData()
+    formData.append("file", file)
 
-    axios.post('http://localhost:3001/upload', formData)
+    axios
+      .post("http://localhost:3001/upload", formData)
       .then((response) => {
-        console.log(response.data);
-        setFile(null);
+        console.log(response.data)
+        setFile(null)
       })
       .catch((error) => {
-        console.error(error);
-      }).finally(() => {
-        axios.get('http://localhost:3001/uploads')
-      .then((response) => {
-        setUploadedFiles(response.data);
+        console.error(error)
       })
-      .catch((error) => {
-        console.error(error);
-      });
+      .finally(() => {
+        axios
+          .get("http://localhost:3001/uploads")
+          .then((response) => {
+            setUploadedFiles(response.data)
+          })
+          .catch((error) => {
+            console.error(error)
+          })
       })
-  };
+  }
 
   const handleDownload = (filename) => {
-    window.open(`http://localhost:3001/download/${filename}`);
-  };
+    window.open(`http://localhost:3001/download/${filename}`)
+  }
 
   return (
-    <div className='container'>
-        <h3>Your Files</h3>
-      <ul className='files-grid'>
-        {uploadedFiles.map((file, index) => (
-          <li key={index}>
-            <AiTwotoneFile className='file-icon' />
-            <button onClick={() => handleDownload(file)}>{file}</button>
+    <div className="container">
+      <div className="header">
+        <h3>
+          Remote<strong>NAS</strong>
+        </h3>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      <ul className="files-grid">
+        {uploadedFiles.map((file) => (
+          <li key={file} onClick={() => handleDownload(file)}>
+            <AiTwotoneFile className="file-icon" />
+            <div onClick={() => handleDownload(file)}>{file}</div>
           </li>
         ))}
       </ul>
       <div className="uploader">
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}><span>Upload</span><AiOutlineCloudUpload /></button>
-      <div>
-        {file && <p>Selected File: {file.name}</p>}
-      </div>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleUpload}>
+          <span>Upload</span>
+          <AiOutlineCloudUpload />
+        </button>
+        {/* <div>{file && <p>Selected File: {file.name}</p>}</div> */}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default FileUploadDownload;
-
+export default FileUploadDownload
